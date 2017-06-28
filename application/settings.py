5bdee8c7b19dd6c353b871141883d3bdba8b2141
Jose Angel <esojangel@gmail.com>
2017-06-28 16:42:12 -0400

    Update settings.py

--- application/settings.py
+++ application/settings.py
@@ -121,14 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'storage'),
-)
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'storage', 'media')
-
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
